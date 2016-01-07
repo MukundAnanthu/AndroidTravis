1d66883987043ef210a400c3283aedad7d99dce6
Mukund A <mukund.code@gmail.com>
2016-01-07 17:39:48 +0530

    MediaPlayer play on LongClick, works

--- app/src/main/java/com/example/admin/sixteen/SoundEffects.java
+++ app/src/main/java/com/example/admin/sixteen/SoundEffects.java
@@ -2,6 +2,7 @@ package com.example.admin.sixteen;
 
 import android.app.Activity;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.view.View;
@@ -9,24 +10,33 @@ import android.view.View;
 /**
  * Created by admin on 07-01-2016.
  */
-public class SoundEffects extends Activity implements View.OnClickListener{
+public class SoundEffects extends Activity implements View.OnClickListener, View.OnLongClickListener {
 
     private SoundPool sp;
     private int explosion = 0;
+    private MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         View v = new View(this);
         v.setOnClickListener(this);
+        v.setOnLongClickListener(this);
         setContentView(v);
         sp = new SoundPool(5, AudioManager.STREAM_MUSIC,0);
         explosion = sp.load(this,R.raw.skyfall,1);
+        mp = MediaPlayer.create(this,R.raw.skyfall);
     }
 
     @Override
     public void onClick(View v) {
         if(explosion != 0 )
             sp.play(explosion,1,1,0,0,1);
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        mp.start();
+        return false;
     }
 }
