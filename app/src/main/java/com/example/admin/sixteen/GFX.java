0cfeaf2758ea9e5f33d362afd6b9ef96f04d469c
Mukund A <mukund.code@gmail.com>
2016-01-07 17:16:30 +0530

    Add wake_lock to prevent the phone from locking itself when user is inactive

--- app/src/main/java/com/example/admin/sixteen/GFX.java
+++ app/src/main/java/com/example/admin/sixteen/GFX.java
@@ -1,7 +1,9 @@
 package com.example.admin.sixteen;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.PowerManager;
 
 /**
  * Created by admin on 06-01-2016.
@@ -9,11 +11,23 @@ import android.os.Bundle;
 public class GFX extends Activity {
 
     private MyBringBack ourView;
-
+    private PowerManager.WakeLock wL;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+        wL = pm.newWakeLock(PowerManager.FULL_WAKE_LOCK,
+                "whatever");
+
         super.onCreate(savedInstanceState);
+        wL.acquire();
         ourView = new MyBringBack(this);
         setContentView(ourView);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        wL.release();
     }
 }
